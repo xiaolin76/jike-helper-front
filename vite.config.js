6fe9658cb6e2814cb5f9e25a96b0f084d1edcc60
xiaolin76 <3312281834@qq.com>
2026-03-14 20:23:24 +0800

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  // 确保构建路径正确
+  base: './'
 })
